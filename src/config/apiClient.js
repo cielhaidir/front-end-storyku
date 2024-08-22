7b4b50477f2d0e177f2ddf9566f95990cfe7be15
cielhaidir <phoenicchi@gmail.com>
2024-08-22 13:08:50 +0800

    fix: update apiCLient to fix api error

--- src/config/apiClient.js
+++ src/config/apiClient.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const apiClient = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
+    timeout: 10000, 
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 export default apiClient;
